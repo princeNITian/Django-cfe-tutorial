a617f78ec2cda82502c5b192c26b212b11fe0b0c
Prince Raj <princeraj427@gmai.com>
2019-10-03 09:40:11 +0530

    Upto Login done- first commit

---
+++ ecommerce/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder":"Your full Name"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget = forms.EmailInput(
+            attrs = {
+                "class":"form-control",
+                "placeholder":"Enter your email"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget = forms.Textarea(
+            attrs = {
+                "class" : "form-control",
+                "placeholder": "Enter Your Content"
+            }
+        )
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail.com.")
+        return email
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
---
+++ ecommerce/views.py
@@ -0,0 +1,96 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .forms import ContactForm,LoginForm
+
+def home_page(request):
+    context = {
+        'title':"Prince Raj",
+        'content': "Welcome to home page"
+    }
+    return render(request,"homepage.html",context)
+
+def about_page(request):
+    context = {
+        'title': "About Me",
+        'content':"Welcome to about page!"
+    }
+    return render(request,"about.html",context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+      'title': "Contact Page",
+      'content': "Welcome to conatact page",
+      'form': contact_form
+    }
+    if contact_form.is_valid():
+      print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #   #print(request.POST)
+    #   print(request.POST.get("fullname"))
+    #   print(request.POST.get("email"))
+    #   print(request.POST.get("content"))
+    return render(request,"contact/view.html",context)
+
+def login_page(request):
+  form = LoginForm(request.POST or None)
+  context = {
+    "form": form
+  }
+  print("User logged in..")
+  #print(request.user.is_authenticated())
+  if form.is_valid():
+    print(form.cleaned_data)
+    username = form.cleaned_data.get("username")
+    password = form.cleaned_data.get("password")
+    user = authenticate(request,username=username,password=password)
+    print(user)
+    if user is not None:
+      #print(request.user.is_authenticated())
+      login(request,user) 
+      # Redirect to a success page
+      #context["form"] = LoginForm()
+      return redirect("/login")
+    else:
+      print("Error")
+    
+  return render(request,"auth/login.html",context)
+
+def register_page(request):
+  form = LoginForm(request.POST or None)
+  context = {
+    "form": form
+  }
+  print(request.user.is_authenticated())
+  print("User logged In")
+  if form.is_valid():
+    print(form.cleaned_data)
+  return render(request,"auth/register.html",context)
+
+def home_page_old(request):
+    html_ = '''
+    <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+'''
+    return HttpResponse(html_)
